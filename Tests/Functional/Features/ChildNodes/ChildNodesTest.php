a5be69e220af90f72614de8428ca5868f15fde4d
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-06-21 18:01:09 +0200

    TASK: Unskip test after merge of core bugfix
    see https://github.com/neos/neos-development-collection/issues/4351

--- Tests/Functional/Features/ChildNodes/ChildNodesTest.php
+++ Tests/Functional/Features/ChildNodes/ChildNodesTest.php
@@ -59,8 +59,6 @@ class ChildNodesTest extends AbstractNodeTemplateTestCase
     /** @test */
     public function itMatchesSnapshot4(): void
     {
-        $this->markTestSkipped('Until https://github.com/neos/neos-development-collection/issues/4351');
-
         $createdNode = $this->createNodeInto(
             $this->homePageMainContentCollectionNode,
             'Flowpack.NodeTemplates:Content.AllowedChildNodes',
